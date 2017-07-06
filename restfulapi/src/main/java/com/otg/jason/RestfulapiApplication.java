e6147892f39be027d0c5025cc9713bf0a2c6e34b
jason <gjf_hot@126.com>
2017-07-06 14:50:33 +0800

    创建一个Restful API

---
+++ restfulapi/src/main/java/com/otg/jason/RestfulapiApplication.java
@@ -0,0 +1,12 @@
+package com.otg.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulapiApplication.class, args);
+	}
+}
---
+++ restfulapi/src/main/java/com/otg/jason/api/entity/User.java
@@ -0,0 +1,61 @@
+package com.otg.jason.api.entity;
+
+/**
+ * Created by jason.guan on 2017/7/6.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private String nickName;
+    private int age;
+    private String type;
+    private String sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
